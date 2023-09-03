11695d01e25cedfc0e1eb2c4f54a860e790687fc
palakgoel30 <gpalak7055@gmail.com>
2023-09-03 18:51:35 +0530

    Leetcode Question 137

---
+++ Single_number_II.py
@@ -0,0 +1,13 @@
+def singleNumber(nums):
+    dict = {}
+    for item in nums:
+        if (item in dict):
+            dict[item] += 1
+        else:
+            dict[item] = 1
+    for key, value in dict.items():
+        if (value == 1):
+            return key
+
+nums = [2,2,3,2]
+print(singleNumber(nums))
